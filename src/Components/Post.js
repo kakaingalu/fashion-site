5f79e3c8b50029bb0ab9808a85179a531857f802
Kochela <josephngalu96@gmail.com>
2024-11-06 21:22:11 +0300

    this version 3 of debugging the crud especially the veiwing of images

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 import ImageGallery from 'react-image-gallery';
 
-const Post = ({ title, content, image }) => {
+const Post = ({ title, content, images }) => {
   const truncateText = (text, maxLength = 150) => {
     if (text.length <= maxLength) return text;
     return `${text.substring(0, maxLength)}...`;
@@ -15,7 +15,9 @@ const Post = ({ title, content, image }) => {
       <Card.Header className='fw-bold text-center'>{title}</Card.Header>
       <Card.Body style={{ maxHeight: '1000px', overflow: 'hidden' }}>
         {/* <Card.Img variant="top" src={image} alt={`Image for ${title}`} fluid style={{ height: '400px' }}/> */}
-        <ImageGallery items={[{ original: image }]}  />
+        {images && images.length > 0 ? (
+          <ImageGallery items={images.map(img => ({ original: img }))} />
+        ) : null}
         <Card.Text >{truncatedContent}</Card.Text>
       </Card.Body>
     </Card>
